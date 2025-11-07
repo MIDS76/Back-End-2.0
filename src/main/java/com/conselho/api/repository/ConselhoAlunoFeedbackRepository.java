183e96ab8c8a457a4ccd2389fa6ebe7c4c324951
henrique_menel <henriqueworks68@gmail.com>
2025-11-06 21:18:33 -0300

    feat: ajustes adicionando validação da existencia do Conselho turma feedback

--- src/main/java/com/conselho/api/repository/ConselhoAlunoFeedbackRepository.java
+++ src/main/java/com/conselho/api/repository/ConselhoAlunoFeedbackRepository.java
@@ -4,4 +4,5 @@ import com.conselho.api.model.ConselhoAlunoFeedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConselhoAlunoFeedbackRepository extends JpaRepository<ConselhoAlunoFeedback, Long> {
+    boolean existsByConselhoId(Long idConselho);
 }
