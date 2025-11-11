b0ba49309aa7b54aafbd372eb966f7d2cf9e570c
HellenGabi <hellen_scarantti@estudante.sesisenai.org.br>
2025-11-11 20:25:47 -0300

    feat: criando endpoint para listar alunos por turma

--- src/main/java/com/conselho/api/controller/AlunoTurmaController.java
+++ src/main/java/com/conselho/api/controller/AlunoTurmaController.java
@@ -6,10 +6,7 @@ import com.conselho.api.service.AlunoTurmaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -30,5 +27,13 @@ public class AlunoTurmaController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.criarAlunoTurma(request));
+    }
+
+    @GetMapping("/listarAlunosPorTurma/{idTurma}")
+    public ResponseEntity<List<String>> listarAlunosPorId(
+            @PathVariable Long idTurma
+    ){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.listarAlunosPorId(idTurma));
     }
 }
--- src/main/java/com/conselho/api/repository/AlunoTurmaRepository.java
+++ src/main/java/com/conselho/api/repository/AlunoTurmaRepository.java
@@ -3,5 +3,9 @@ package com.conselho.api.repository;
 import com.conselho.api.model.AlunoTurma;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlunoTurmaRepository extends JpaRepository<AlunoTurma,Long> {
+
+    List<AlunoTurma> findByTurmaId(Long idTurma);
 }
