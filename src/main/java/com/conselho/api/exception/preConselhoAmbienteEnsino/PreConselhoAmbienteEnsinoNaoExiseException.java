cd3d0658c96aced1933eeb2a72f8b026b028062b
SGA-OKC <vitorlo2006eduardo@gmail.com>
2025-11-11 19:25:18 -0300

    feat: ajustes no fluxo do pre-conselho ambiente ensino

--- src/main/java/com/conselho/api/exception/preConselhoAmbienteEnsino/PreConselhoAmbienteEnsinoNaoExiseException.java
+++
@@ -1,7 +0,0 @@
-package com.conselho.api.exception.preConselhoAmbienteEnsino;
-
-public class PreConselhoAmbienteEnsinoNaoExiseException extends RuntimeException{
-    public PreConselhoAmbienteEnsinoNaoExiseException() {
-        super("Pré Conselho ambiente ensino não encontrado.");
-    }
-}
--- src/main/java/com/conselho/api/service/preConselho/PreConselhoAmbienteEnsinoService.java
+++ src/main/java/com/conselho/api/service/preConselho/PreConselhoAmbienteEnsinoService.java
@@ -3,10 +3,12 @@ package com.conselho.api.service.preConselho;
 import com.conselho.api.dto.mapper.preConselho.PreConselhoAmbienteEnsinoMapper;
 import com.conselho.api.dto.request.preConselho.PreConselhoAmbienteEnsinoRequestDTO;
 import com.conselho.api.dto.response.preConselho.PreConselhoAmbienteEnsinoResponseDTO;
-import com.conselho.api.exception.preConselhoAmbienteEnsino.PreConselhoAmbienteEnsinoJaExiseException;
-import com.conselho.api.exception.preConselhoAmbienteEnsino.PreConselhoAmbienteEnsinoNaoExiseException;
+import com.conselho.api.exception.preConselho.PreConselhoNaoExisteException;
+import com.conselho.api.exception.preConselhoAmbienteEnsino.PreConselhoAmbienteEnsinoJaExisteException;
+import com.conselho.api.exception.preConselhoAmbienteEnsino.PreConselhoAmbienteEnsinoNaoExisteException;
 import com.conselho.api.model.preConselho.PreConselhoAmbienteEnsino;
 import com.conselho.api.repository.preConselho.PreConselhoAmbienteEnsinoRepository;
+import com.conselho.api.repository.preConselho.PreConselhoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,19 +19,19 @@ import java.util.List;
 public class PreConselhoAmbienteEnsinoService {
     private PreConselhoAmbienteEnsinoRepository repository;
     private PreConselhoAmbienteEnsinoMapper mapper;
+    private PreConselhoRepository preConselhoRepository;
 
     public PreConselhoAmbienteEnsinoResponseDTO criarPreConselhoAmbienteEnsino(PreConselhoAmbienteEnsinoRequestDTO requestDTO){
-        PreConselhoAmbienteEnsino preConselhoAmbienteEnsino = repository.findById(requestDTO.idPreConselho())
-                .orElseThrow(PreConselhoAmbienteEnsinoNaoExiseException::new);
-
-        if (repository.existsByPreConselhoId(requestDTO.idPreConselho())) {
-            throw new PreConselhoAmbienteEnsinoJaExiseException();
-        }
         PreConselhoAmbienteEnsino preConAmbienteEnsino = mapper.paraEntidade(requestDTO);
-        preConAmbienteEnsino.setId(preConselhoAmbienteEnsino.getId());
+
+        preConAmbienteEnsino.setPreConselho(preConselhoRepository.findById(requestDTO.idPreConselho())
+                .orElseThrow(PreConselhoNaoExisteException::new));
+
+        preConAmbienteEnsino.setPontosPositivos(requestDTO.pontosPositivos());
+        preConAmbienteEnsino.setPontosMelhoria(requestDTO.pontosMelhoria());
+        preConAmbienteEnsino.setSugestoes(requestDTO.sugestoes());
 
         PreConselhoAmbienteEnsino salvarPreConselhoAmbienteEnsino= repository.save(preConAmbienteEnsino);
-
         return mapper.paraResposta(salvarPreConselhoAmbienteEnsino);
     }
 
@@ -42,23 +44,23 @@ public class PreConselhoAmbienteEnsinoService {
 
     public PreConselhoAmbienteEnsinoResponseDTO buscarPorId(Long id) {
         PreConselhoAmbienteEnsino preConselhoAmbienteEnsino = repository.findById(id)
-                .orElseThrow(PreConselhoAmbienteEnsinoNaoExiseException::new);
+                .orElseThrow(PreConselhoAmbienteEnsinoNaoExisteException::new);
 
         return mapper.paraResposta(preConselhoAmbienteEnsino);
     }
 
     public PreConselhoAmbienteEnsinoResponseDTO atualizarPreConselhoAmbienteEnsino(Long id, PreConselhoAmbienteEnsinoRequestDTO requestDTO) {
         PreConselhoAmbienteEnsino preConselhoAmbienteEnsino = repository.findById(id)
-                .orElseThrow(PreConselhoAmbienteEnsinoNaoExiseException::new);
+                .orElseThrow(PreConselhoAmbienteEnsinoNaoExisteException::new);
 
-        PreConselhoAmbienteEnsino preConselhoAmbienteEnsinoAtualizado = mapper.verificarUpdate(requestDTO,preConselhoAmbienteEnsino);
-
+        PreConselhoAmbienteEnsino preConselhoAmbienteEnsinoAtualizado = mapper.paraUpdate(requestDTO,preConselhoAmbienteEnsino);
         return mapper.paraResposta(preConselhoAmbienteEnsinoAtualizado);
     }
 
     public void deletarPreConselhoAmbienteEnsino(Long id) {
         if (!repository.existsById(id)) {
-            throw new PreConselhoAmbienteEnsinoNaoExiseException();
+            throw new PreConselhoAmbienteEnsinoNaoExisteException();
         }
+        repository.deleteById(id);
     }
 }
