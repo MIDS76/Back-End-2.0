f55350452d0b5e48d79c6d751a629aa34e384d0a
SGA-OKC <vitorlo2006eduardo@gmail.com>
2025-11-12 14:21:51 -0300

    feat: correção do endpoint ucProfessor que antes se era ucTurma

---
+++ src/main/java/com/conselho/api/controller/UcProfessorController.java
@@ -0,0 +1,59 @@
+package com.conselho.api.controller;
+
+import com.conselho.api.dto.request.UcProfessorRequestDTO;
+import com.conselho.api.dto.response.UcProfessorResponseDTO;
+import com.conselho.api.service.UcProfessorService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ucprofessor")
+@AllArgsConstructor
+public class UcProfessorController {
+
+    private UcProfessorService ucProfessorService;
+
+    @PostMapping("/criar")
+    public ResponseEntity<UcProfessorResponseDTO> criarUcProfessor(
+            @Valid @RequestBody UcProfessorRequestDTO requestDTO
+    ){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ucProfessorService.criarUcProfessor(requestDTO));
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<UcProfessorResponseDTO>> listarTodos() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ucProfessorService.listarUcProfessor());
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<UcProfessorResponseDTO> buscarUcProfessorPorId(
+            @PathVariable Long id
+    ){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ucProfessorService.buscarUcProfessorPorId(id));
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<UcProfessorResponseDTO> atualizarUcProfessor(
+            @PathVariable Long id,
+            @Valid @RequestBody UcProfessorRequestDTO requestDTO
+    ){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ucProfessorService.atualizarUcProfessor(requestDTO,id));
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletarUcProfessor(
+            @PathVariable Long id
+    ){
+        ucProfessorService.deletarUcProfessor(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+}
--- src/main/java/com/conselho/api/dto/request/UcProfessorRequestDTO.java
+++ src/main/java/com/conselho/api/dto/request/UcProfessorRequestDTO.java
@@ -3,7 +3,7 @@ package com.conselho.api.dto.request;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record UcTurmaRequestDTO(
+public record UcProfessorRequestDTO(
         @NotNull(message = "O id deve ser informado.")
         @Positive(message = "O id não pode ser negativo.")
         Long idConselho,
