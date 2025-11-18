774f7219bab4f16fba04648d33d50af6bf3562aa
SGA-OKC <vitorlo2006eduardo@gmail.com>
2025-11-18 14:22:27 -0300

    feat: ajustando alguns teste de integração

--- src/test/java/com/conselho/api/testeIntegracao/UcProfessorTesteIntegracao.java
+++ src/test/java/com/conselho/api/testeIntegracao/UcProfessorTesteIntegracao.java
@@ -1,74 +1,75 @@
-//package com.conselho.api.testeIntegracao;
-//
-//import com.conselho.api.dto.request.UcProfessorRequestDTO;
-//import com.conselho.api.dto.response.UcProfessorResponseDTO;
-//import com.conselho.api.exception.ucProfessor.UcProfessorExisteException;
-//import com.conselho.api.model.UcProfessor;
-//import com.conselho.api.repository.UcProfessorRepository;
-//import com.conselho.api.service.UcProfessorService;
-//import jakarta.transaction.Transactional;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.TestConstructor;
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//
-//@SpringBootTest
-//@Transactional
-//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-//public class UcProfessorTesteIntegracao {
-//    private final UcProfessorService ucProfessorService;
-//    private final UcProfessorRepository ucProfessorRepository;
-//
-//    public UcProfessorTesteIntegracao (
-//            UcProfessorService ucProfessorService,
-//            UcProfessorRepository ucProfessorRepository
-//    ) {
-//        this.ucProfessorService = ucProfessorService;
-//        this.ucProfessorRepository = ucProfessorRepository;
-//    }
-//
-//    @BeforeEach
-//    void setup() {
-//        UcProfessor ucProfessor = new UcProfessor();
-//
-//        ucProfessor.setId(1L);
-//        ucProfessorRepository.save(ucProfessor);
-//    }
-//
-//    @Test
-//    void deveCriarUcProfessorComSucesso() {
-//        UcProfessorRequestDTO requestDTO = new UcProfessorRequestDTO(1L, 2L, 3L);
-//        UcProfessorResponseDTO responseDTO = ucProfessorService.criarUcProfessor(requestDTO);
-//
-//        assertThat(responseDTO).isNotNull();
-//        assertThat(responseDTO.id()).isNotNull();
-//        assertThat(responseDTO.idProfessor()).isNotNull();
-//        assertThat(responseDTO.idConselho()).isNotNull();
-//        assertThat(responseDTO.idUnidadeCurricular()).isNotNull();
-//    }
-//
+package com.conselho.api.testeIntegracao;
+
+import com.conselho.api.dto.request.UcProfessorRequestDTO;
+import com.conselho.api.dto.response.UcProfessorResponseDTO;
+import com.conselho.api.exception.ucProfessor.UcProfessorExisteException;
+import com.conselho.api.model.UcProfessor;
+import com.conselho.api.repository.UcProfessorRepository;
+import com.conselho.api.service.UcProfessorService;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Transactional
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public class UcProfessorTesteIntegracao {
+    private final UcProfessorService ucProfessorService;
+    private final UcProfessorRepository ucProfessorRepository;
+    private UcProfessor ucProfessor;
+
+    public UcProfessorTesteIntegracao (
+            UcProfessorService ucProfessorService,
+            UcProfessorRepository ucProfessorRepository
+    ) {
+        this.ucProfessorService = ucProfessorService;
+        this.ucProfessorRepository = ucProfessorRepository;
+    }
+
+    @BeforeEach
+    void setup() {
+        UcProfessor ucProfessor = new UcProfessor();
+
+        ucProfessor.setId(1L);
+        ucProfessorRepository.save(ucProfessor);
+    }
+
+    @Test
+    void deveCriarUcProfessorComSucesso() {
+        UcProfessorRequestDTO requestDTO = new UcProfessorRequestDTO(1L, 6L, 2L);
+        UcProfessorResponseDTO responseDTO = ucProfessorService.criarUcProfessor(requestDTO);
+
+        assertThat(responseDTO).isNotNull();
+        assertThat(responseDTO.id()).isNotNull();
+        assertThat(responseDTO.idProfessor()).isNotNull();
+        assertThat(responseDTO.idConselho()).isNotNull();
+        assertThat(responseDTO.idUnidadeCurricular()).isNotNull();
+    }
+
 //    @Test
 //    void deveListarUcProfessorComSucesso() {
 //        var ucProfessor = ucProfessorService.listarUcProfessor();
 //
 //        Assertions.assertThat(ucProfessor).isNotEmpty();
 //    }
-//
-//    @Test
-//    void deveBuscarUcProfessorPorIdComSucesso() {
-//        UcProfessorRequestDTO requestDTO = new UcProfessorRequestDTO(1L,2L,3L);
-//        UcProfessorResponseDTO responseDTO = ucProfessorService.criarUcProfessor(requestDTO);
-//        UcProfessorResponseDTO response = ucProfessorService.buscarUcProfessorPorId(responseDTO.id());
-//
-//        assertThat(response).isNotNull();
-//        assertThat(response.id()).isEqualTo(responseDTO.id());
-//        assertThat(response.idProfessor()).isEqualTo(responseDTO.idProfessor());
-//        assertThat(response.idConselho()).isEqualTo(responseDTO.idConselho());
-//        assertThat(response.idUnidadeCurricular()).isEqualTo(responseDTO.idUnidadeCurricular());
-//    }
-//
+
+    @Test
+    void deveBuscarUcProfessorPorIdComSucesso() {
+        UcProfessorRequestDTO requestDTO = new UcProfessorRequestDTO(1L,5L,2L);
+        UcProfessorResponseDTO responseDTO = ucProfessorService.criarUcProfessor(requestDTO);
+        UcProfessorResponseDTO response = ucProfessorService.buscarUcProfessorPorId(responseDTO.id());
+
+        assertThat(response).isNotNull();
+        assertThat(response.id()).isEqualTo(responseDTO.id());
+        assertThat(response.idProfessor()).isEqualTo(responseDTO.idProfessor());
+        assertThat(response.idConselho()).isEqualTo(responseDTO.idConselho());
+        assertThat(response.idUnidadeCurricular()).isEqualTo(responseDTO.idUnidadeCurricular());
+    }
+
 //    @Test
 //    void deveAtualizarUcProfessorComSucesso() {
 //        UcProfessorRequestDTO requestDTO = new UcProfessorRequestDTO(4L,6L,2L);
@@ -79,22 +80,24 @@
 //        assertThat(response).isNotNull();
 //        assertThat(response.id()).isEqualTo(2);
 //    }
-//
-//    @Test
-//    void deveDeletarUcProfessor() {
-//        UcProfessorRequestDTO requestDTO = new UcProfessorRequestDTO(1L,3L,4L);
-//        UcProfessorResponseDTO responseDTO = ucProfessorService.criarUcProfessor(requestDTO);
+
+    @Test
+    void deveDeletarUcProfessor() {
+        ucProfessorService.deletarUcProfessor(ucProfessor.getId());
+
+        assertThat(ucProfessorRepository.findById(ucProfessor.getId())).isEmpty();
+//        UcProfessorResponseDTO responseDTO = new UcProfessorResponseDTO(1L,1L,3L,"Valentim", 2L,"Banco de Dados");
 //        ucProfessorService.deletarUcProfessor(responseDTO.id());
 //
 //        assertThat(ucProfessorRepository.findById(responseDTO.id())).isEmpty();
-//    }
-//
+    }
+
 //    @Test
 //    void naoDeveCriarUcProfessorComNomeExistente() {
-//        UcProfessorRequestDTO requestDTO = new UcProfessorRequestDTO(2L,4L,5L);
+//        UcProfessorRequestDTO requestDTO = new UcProfessorRequestDTO(1L,6L,1L);
 //        ucProfessorService.criarUcProfessor(requestDTO);
 //
 //        org.junit.jupiter.api.Assertions.assertThrows(UcProfessorExisteException.class, () ->
 //                ucProfessorService.criarUcProfessor(requestDTO));
 //    }
-//}
+}
