ba22a26b727aceeb7d4e76ccb547b7f42fae4f4c
JOANA JENSEN SCHIFTER <joana_schifter@estudante.sesisenai.org.br>
2025-11-18 20:07:09 -0300

    feat: implementando swagger nos controllers de pré-conselho

--- src/main/java/com/conselho/api/controller/preConselho/PreConselhoSupervisaoController.java
+++ src/main/java/com/conselho/api/controller/preConselho/PreConselhoSupervisaoController.java
@@ -3,6 +3,9 @@ package com.conselho.api.controller.preConselho;
 import com.conselho.api.dto.request.preConselho.PreConselhoSupervisaoRequestDTO;
 import com.conselho.api.dto.response.preConselho.PreConselhoSupervisaoResponseDTO;
 import com.conselho.api.service.preConselho.PreConselhoSupervisaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,17 @@ public class PreConselhoSupervisaoController {
 
     private PreConselhoSupervisaoService service;
 
+    @Operation(
+            summary = "Cria um novo pré-conselho sobre os supervisores.",
+            description = "Este endpoint cria um pré-conselho sobre os supervisores, com base nos dados fornecidos."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pré-conselho criado com sucesso!"),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido. Verifique suas permissões ou entre em contato com o administrador."),
+            @ApiResponse(responseCode = "404", description = "Nenhum supervisor encontrado para criar o pré-conselho."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor. Por favor, tente novamente mais tarde.")
+    })
+
     @PostMapping("/criar")
     public ResponseEntity<PreConselhoSupervisaoResponseDTO> criarPreConselhoSupervisao (
         @Valid @RequestBody PreConselhoSupervisaoRequestDTO requestDTO
@@ -26,11 +40,33 @@ public class PreConselhoSupervisaoController {
                 .body(service.criarPreConselhoSupervisao(requestDTO));
     }
 
+    @Operation(
+            summary = "Lista todos os pré-conselhos sobre os supervisores.",
+            description = "Este endpoint retorna todos os pré-conselhos sobre os supervisores cadastrados no sistema."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pré-conselhos encontrados com sucesso!"),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido. Verifique suas permissões ou entre em contato com o administrador."),
+            @ApiResponse(responseCode = "404", description = "Nenhum pré-conselho encontrado."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor. Por favor, tente novamente mais tarde.")
+    })
+
     @GetMapping("/listar")
     public ResponseEntity<List<PreConselhoSupervisaoResponseDTO>> listarPreConselhoSupervisao() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(service.listarTodos());
     }
+
+    @Operation(
+            summary = "Busca um pré-conselho pelo ID.",
+            description = "Este endpoint retorna as informações de pré-conselho sobre os supervisores cadastrado no sistema, baseado no ID fornecido."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pré-conselho encontrado com sucesso!"),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido. Verifique suas permissões ou entre em contato com o administrador."),
+            @ApiResponse(responseCode = "404", description = "Nenhum pré-conselho encontrado com o ID fornecido."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor. Por favor, tente novamente mais tarde.")
+    })
 
     @GetMapping("/buscar/{id}")
     public ResponseEntity<PreConselhoSupervisaoResponseDTO> buscarPreConselhoSupervisaoPorId (
@@ -40,6 +76,18 @@ public class PreConselhoSupervisaoController {
                 .body(service.buscarPorId(id));
     }
 
+    @Operation(
+            summary = "Atualiza um pré-conselho a partir do ID.",
+            description = "Este endpoint atualiza as informações de um pré-conselho sobre os supervisores cadastrado no sistema, baseado no ID fornecido."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pré-conselho atualizado com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos no corpo da requisição."),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido. Verifique suas permissões ou entre em contato com o administrador."),
+            @ApiResponse(responseCode = "404", description = "Nenhum pré-conselho encontrado com o ID fornecido."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor. Por favor, tente novamente mais tarde.")
+    })
+
     @PutMapping("/atualizar/{id}")
     public ResponseEntity<PreConselhoSupervisaoResponseDTO> atualizarPreConselhoSupervisaoPorId (
         @PathVariable Long id,
@@ -48,6 +96,17 @@ public class PreConselhoSupervisaoController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(service.atualizarPreConselhoSupervisao(id,requestDTO));
     }
+
+    @Operation(
+            summary = "Deleta um pré-conselho a partir do ID.",
+            description = "Este endpoint deleta as informações de um pré-conselho sobre os supervisores cadastrado no sistema, baseado no ID fornecido."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Pré-conselho deletado com sucesso!"),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido. Verifique suas permissões ou entre em contato com o administrador."),
+            @ApiResponse(responseCode = "404", description = "Nenhum pré-conselho encontrado com o ID fornecido."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor. Por favor, tente novamente mais tarde.")
+    })
 
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<Void> deletarPreConselhoSupervisao (
